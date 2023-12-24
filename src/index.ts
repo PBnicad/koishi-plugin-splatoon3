4c8b700f0614c3a5fd4eb33026d6c27db2ed2373
Ylongs <ysl4936@126.com>
2023-12-24 22:15:10 +0800

    [feature]: only display the latest two splatfests

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
   // 定义一个函数来处理截图逻辑
-  async function captureScreenshot(url: string, filename: string, viewport: { width: number, height: number }) {
+  async function captureScreenshot(url: string, filename: string, viewport: { width: number, height: number }, clip?: { x: number, y: number, width: number, height: number }) {
     const page = await ctx.puppeteer.page();
     await page.setViewport(viewport);
     await page.goto(url, { waitUntil: 'networkidle0' });
@@ -21,7 +21,8 @@ export function apply(ctx: Context) {
     fs.mkdirSync(screenshotsDir, { recursive: true });
   
     const screenshotPath = path.join(screenshotsDir, filename);
-    await page.screenshot({ path: screenshotPath, fullPage: true });
+    const screenshotOptions = clip ? { path: screenshotPath, clip } : { path: screenshotPath, fullPage: true };
+    await page.screenshot(screenshotOptions);
   
     const imageBuffer = fs.readFileSync(screenshotPath);
     const imageBase64 = imageBuffer.toString('base64');
@@ -36,6 +37,14 @@ export function apply(ctx: Context) {
   const challengesViewport = { width: 1200, height: 700 };
   const gearViewport = { width: 1100, height: 800 };
   const splatfestsViewport = { width: 1200, height: 900 };
+
+  // “喷喷祭典”命令的视窗设置
+  const splatfestsClip = {
+    x: 0, // 您需要决定的 X 坐标
+    y: 0, // 您需要决定的 Y 坐标
+    width: 1200, // 您需要决定的宽度
+    height: 950 // 您需要决定的高度
+  };
 
   // “喷喷地图”命令
   ctx.command('喷喷地图', '获取Splatoon3地图界面截图')
@@ -61,9 +70,9 @@ export function apply(ctx: Context) {
       return captureScreenshot('https://splatoon3.ink/gear', 'splatoon3_gear_screenshot.png', gearViewport);
     });
 
-  // “喷喷祭典”命令
+  // “喷喷祭典”命令，这次传入 clip 参数
   ctx.command('喷喷祭典', '获取Splatoon3祭典界面截图')
     .action(async ({ session }) => {
-      return captureScreenshot('https://splatoon3.ink/splatfests', 'splatoon3_splatfests_screenshot.png', splatfestsViewport);
+      return captureScreenshot('https://splatoon3.ink/splatfests', 'splatoon3_splatfests_screenshot.png', splatfestsViewport, splatfestsClip);
     });
 }
